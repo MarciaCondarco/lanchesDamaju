802ea9887e7963fcf8e448ddb9499fca1689c37d
Marcy_ <marciagisseli21@gmail.com>
2025-04-14 04:56:51 -0300

    commit

--- lanchesDamaju/conexaoBanco.cs
+++ lanchesDamaju/conexaoBanco.cs
@@ -2,7 +2,13 @@
 
 public static class ConexaoBanco
 {
-    private static readonly string connectionString = "Server=srv1438.hstgr.io;Port=3306;Database=u289366797_db_damaju;Uid=u289366797_Damaju;Pwd=Damaju123&;";
+    private static readonly string connectionString = 
+        "Server=srv1438.hstgr.io;" +
+        "Port=3306;" +
+        "Database=u289366797_db_damaju;" +
+        "Uid=u289366797_Damaju;" +
+        "Pwd=Damaju123&;"+
+        "convert zero datetime=True";
 
     public static MySqlConnection ObterConexao()
     {
